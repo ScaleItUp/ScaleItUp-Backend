93825397d8b119194f5cb4fde8edd78d7f54a45b
Arghadeep Chakraborty <argho.chakr@gmail.com>
2023-11-26 19:50:37 -0600

    Trying to deploy on Render

--- app/app.py
+++ app/app.py
@@ -152,4 +152,8 @@ def fetchWeightsFromDB():
         return resultObject
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+def create_app():
+   return app
