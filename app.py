56f8985bae4a8c6d8c56558e45f40cadebea70a5
Arghadeep Chakraborty <argho.chakr@gmail.com>
2023-11-27 05:17:49 -0600

    Added Post Call. Reconfigured the return objects.

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
+#imports
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from pymongo.errors import PyMongoError
 from enum import Enum
 
+#app
 app = Flask(__name__)
 
+#config
 MONGO_URI = f'mongodb+srv://mini-project:CMZTBb6mt6EF1H6s@miniprojects.kb2cknx.mongodb.net/?retryWrites=true&w=majority'
 DB_NAME = 'ScaleKit'
 WEIGHTS_COLLECTION = 'Weights'
 
+#enums
 class StatusCode(Enum):
     SUCCESS = 200
+    CREATION_SUCCESS = 201
     BAD_REQUEST = 400
     NOT_FOUND = 404
     DATABASE_ERROR = 700
@@ -55,12 +60,22 @@ def createErrorObject(statusCode, message):
             'message': 'Failure'
         }
 
-# creates a return object for get call of weights
-def createWeightObject(statusCode, weights, message):
+# creates a return object for get call of weight
+def createWeightObject(statusCode, weight, message):
     return {
         'code': statusCode,
         'data': {
-            'weights': weights,
+            'weight': weight,
+            'message': message
+        },
+        'message': 'Success'
+    }
+
+# creates a success object with a message and status code
+def createSuccessObject(statusCode, message):
+    return {
+        'code': statusCode,
+        'data': {
             'message': message
         },
         'message': 'Success'
@@ -72,12 +87,10 @@ def hello_world():
     return 'Hello, World!'
 
 
-# route for getting weights
-@app.route('/api/get-weights', methods=['GET'])
-# TODO:
-# @jwt_required()
-def getWeights():
-    result = fetchWeightsFromDB()
+# route for getting weight
+@app.route('/api/get-weight', methods=['GET'])
+def getWeight():
+    result = fetchWeightFromDB()
 
     if result['isError']:
         returnObject = createErrorObject(
@@ -85,13 +98,13 @@ def getWeights():
         return jsonify(returnObject), result['statusCode']
     else:
         returnObject = createWeightObject(
-            statusCode=result['statusCode'], weights=result['weights'], message=result['message'])
+            statusCode=result['statusCode'], weight=result['weight'], message=result['message'])
         return jsonify(returnObject), result['statusCode']
     
 
 # fetches data from the DB
 # returns the result object or error object
-def fetchWeightsFromDB():
+def fetchWeightFromDB():
     try:
         # Connect to database
         client = MongoClient(MONGO_URI)
@@ -104,21 +117,15 @@ def fetchWeightsFromDB():
 
         # check if some data is present
         if len(weightsFromDB) > 0:
-            # use this empty array for the return object to be sent
-            weights = []
-            for weightType in weightsFromDB:
-                # if any of these keys are not present, close client and throw exception
-                if not weightsCollection.find({'type': {'$exists': True}}) or not weightsCollection.find({'unit_weight': {'$exists': True}}) or not weightsCollection.find({'total_weight': {'$exists': True}}):
-                    client.close()
-                # add these objects in an array for the response
-                else:
-                    object = {
-                        'type': weightType['type'],
-                        'unit_weight': weightType['unit_weight'],
-                        'total_weight': weightType['total_weight']
-                    }
-                    weights.append(object)
-        # if both hardwares are not present, throw error
+            # get the latest weight from db
+            weight = weightsFromDB.pop()
+            # if any of these keys are not present, close client and throw exception
+            if not weightsCollection.find({'weight': {'$exists': True}}):
+                client.close()
+            # create the object for the response
+            else:
+                object = weight['weight']
+        # if keys are not present, throw error
         else:
             client.close()
             resultObject = {
@@ -133,8 +140,8 @@ def fetchWeightsFromDB():
         resultObject = {
             'isError': False,
             'statusCode': 200,
-            'weights': weights,
-            'message': 'Weights Data fetched successfully!'
+            'weight': object,
+            'message': 'Weight Data fetched successfully!'
         }
         return resultObject
 
@@ -156,3 +163,81 @@ def fetchWeightsFromDB():
             'message': 'Server error: ' + str(e)
         }
         return resultObject
+
+# route for adding a new weight object
+@app.route('/api/send-weight', methods=['POST'])
+def create_project():
+
+    # storing request project
+    weight = request.json['weight']
+
+    result = addWeight(weight)
+
+    if result['isError']:
+        returnObject = createErrorObject(
+            statusCode=result['statusCode'], message=result['message'])
+        return jsonify(returnObject), result['statusCode']
+    else:
+        returnObject = createSuccessObject(
+            statusCode=result['statusCode'], message=result['message'])
+        return jsonify(returnObject), result['statusCode']
+
+
+def addWeight(weight):
+    try:
+        # Connect to database
+        client = MongoClient(MONGO_URI)
+        db = client[DB_NAME]
+        # getting the project collection
+        weightsCollection = db[WEIGHTS_COLLECTION]
+
+        # check if the collection is empty or not
+        if len(list(weightsCollection.find())) == 0:
+            resultObject = {
+                'isError': True,
+                'statusCode': 404,
+                'message': 'Data not found!'
+            }
+            return resultObject
+
+        # create a new weight object
+        newWeight = {
+            'weight': weight
+        }
+
+        # insert weight in the Weights collection
+        weightsCollection.insert_one(newWeight)
+
+        client.close()
+        # return the success object
+        resultObject = {
+            'isError': False,
+            'statusCode': 201,
+            'message': 'Weight added successfully!',
+        }
+        return resultObject
+
+    except PyMongoError as e:
+        # Handle database-related errors
+        client.close()
+        resultObject = {
+            'isError': True,
+            'statusCode': 700,
+            'message': 'Database error: ' + str(e)
+        }
+        return resultObject
+
+    except Exception as e:
+        # Handle other exceptions
+        client.close()
+        resultObject = {
+            'isError': True,
+            'statusCode': 500,
+            'message': 'Server error: ' + str(e)
+        }
+        return resultObject
+
+
+# TODO: Remove for deployment
+# if __name__ == '__main__':
+#     app.run(debug=True)
